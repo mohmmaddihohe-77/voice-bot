e235108793cf987d62ac9a32528c8e3d7bcaf8a8
mohmmaddihohe-77 <mohmmadhamode80@gmail.com>
2026-02-28 01:35:50 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates]
+});
+
+const TOKEN = process.env.TOKEN;
+const GUILD_ID = process.env.GUILD_ID;
+const CHANNEL_ID = process.env.CHANNEL_ID;
+
+client.once('ready', async () => {
+  console.log(`Logged in as ${client.user.tag}`);
+
+  const guild = await client.guilds.fetch(GUILD_ID);
+  const channel = await guild.channels.fetch(CHANNEL_ID);
+
+  joinVoiceChannel({
+    channelId: channel.id,
+    guildId: guild.id,
+    adapterCreator: guild.voiceAdapterCreator,
+    selfDeaf: true,
+    selfMute: true
+  });
+
+  console.log('Joined voice channel.');
+});
+
+client.login(TOKEN);
